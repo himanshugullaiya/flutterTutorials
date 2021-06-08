4faf81c1e7f4373a1e5c6177b78fc3d358492722
Himanshu Gullaiya <himanshugullaiya@gmail.com>
2021-06-08 16:58:01 +0530

    Day 2 Completed

--- flutter_catalog/lib/main.dart
+++ flutter_catalog/lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/homepage.dart';
+import 'homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({Key ke}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to werweb 30 Days of Flutter"),
-          ),
-        ),
-      ),
-    );
+      home: HomePage(),
+    ); //MaterialApp
   }
 }
